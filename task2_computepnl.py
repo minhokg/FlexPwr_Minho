9addaa32233420fcc1f36c431c80873271cc8391
Minho Kang <rkdalsgh32@gmail.com>
2024-03-10 12:26:57 +0100

    Modify the file

--- task2_computepnl.py
+++ task2_computepnl.py
@@ -27,13 +27,3 @@ def compute_pnl(strategy_id: str, database_path: str, table_name: str) -> float:
 
     except sqlite3.Error as e:
         print(f"SQLite error : {e}")
-
-
-# Try the function on strategy_1 with epex_12_20_12_13 table in trades database
-
-try:
-    pnl_1 = compute_pnl(strategy_id="strategy_1", database_path="trades.sqlite",
-                        table_name="epex_12_20_12_13")
-    print(f"PnL of Starategy 1 is {pnl_1} euros")
-except Exception as e:
-    print(f"An error occurred: {e}")
--- task3_pnlapp.py
+++ task3_pnlapp.py
@@ -1,28 +1,26 @@
-from flask import Flask, render_template, jsonify, request, redirect, url_for
+from flask import Flask
+from flask_restx import Api, Resource, reqparse
 from task2_computepnl import compute_pnl  # Import the function in task 2
 from datetime import datetime
 
 app = Flask(__name__)
+api = Api(app, title="Energy Trading API")
+
+parser = reqparse.RequestParser()
+parser.add_argument('strategy_id', type=str, required=True,
+                    help='string identifier of a strategy.')  # Modified description
 
 
-@app.route('/')
-def main():
-    # Show the JSON file of startegy_1 as an example for the initial page
-    return redirect(url_for('get_pnl', strategy_id='strategy_1'))
+@api.route('/pnl/<strategy_id>')
+class PNL(Resource):
+    @api.doc(summary="Return")
+    def get(self, strategy_id):
+        pnl_value = compute_pnl(strategy_id=strategy_id, database_path='trades.sqlite',
+                                table_name='epex_12_20_12_13')
 
-
-# Customize the last url address like 'strategy_1' or 'strategy_2'
-@app.route('/v1/pnl/<string:strategy_id>', methods=['GET'])
-def get_pnl(strategy_id):
-    """
-    Return: JSON file about PnL value and its information
-    """
-    pnl_value = compute_pnl(strategy_id=strategy_id, database_path='trades.sqlite',
-                            table_name='epex_12_20_12_13')
-    response_data = {
-        'strategy': strategy_id,
-        'value': pnl_value,
-        'unit': 'euro',
-        'capture_time': datetime.utcnow().isoformat()  # Use the current time
-    }
-    return jsonify(response_data)
+        return {
+            'strategy': strategy_id,
+            'value': pnl_value,
+            'unit': 'euro',
+            'capture_time': datetime.utcnow().isoformat()  # Use the current time
+        }
