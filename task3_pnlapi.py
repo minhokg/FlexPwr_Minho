89ce4d28c0e0fcd2936e5e5d6e56ad6394e3e2c7
Minho Kang <rkdalsgh32@gmail.com>
2024-03-10 16:22:34 +0100

    Modify the API

---
+++ task3_pnlapi.py
@@ -0,0 +1,40 @@
+
+from flask import Flask
+from task2_computepnl import compute_pnl  # Import the function in task 2
+from flask_restx import Api, Resource, reqparse, fields
+from datetime import datetime
+
+app = Flask(__name__)
+api = Api(app, title="Energy Trading API",
+          prefix='/v1',  # Set base_url as v1
+          default="Get PnL data", default_label="")
+# Define the response data schema with examples
+pnl_model = api.model('schema', {
+    'strategy': fields.String(example='strategy_1'),
+    'value': fields.Float(example=-50),
+    'unit': fields.String(example='euro'),
+    'capture_time': fields.String(example='2024-03-10T14:33:58.408345')
+})
+
+# Build API
+
+
+@api.route('/pnl/<strategy_id>')
+class PNL(Resource):
+    @api.doc(params={'strategy_id': 'string identifier of a strategy.'})
+    @api.response(200, 'A PnL data object.', pnl_model)
+    def get(self, strategy_id):
+        """
+        Returns the pnl of the corresponding strategy.
+        """
+        pnl_value = compute_pnl(strategy_id=strategy_id, database_path='trades.sqlite',
+                                table_name='epex_12_20_12_13')
+        response = {'strategy_id': strategy_id,
+                    'value': pnl_value,
+                    'unit': 'euro',
+                    'capture_time': datetime.utcnow().isoformat()}
+        return response
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
